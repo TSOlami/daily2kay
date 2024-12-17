46deaa7a037e66d7b1176560c211bdfd5329342b
TSOlami <tijani.saheed@yahoo.com>
2024-12-17 15:20:54 +0100

    Add Head component to include Google Ads script for ad integration

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        async
+        src="https://pagead2.googlesyndecode.com/pagead/js/adsbygoogle.js?client=ca-pub-3857857958188415"
+        crossOrigin="anonymous"
+      />
+    </>
+  )
+} 
